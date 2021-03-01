8e0eacf57769630e6fe3a23f8a9d5d1b9065b914
Manu0225 <manu.manu34@hotmail.fr>
2021-03-01 11:26:35 +0100

    updated prop1

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import numpy as np
 from proposition1 import *
 
 
-# unique, counts = np.unique(sample, return_counts=True)
-
-
-# print(dict(zip(unique, counts)))
-
-
-#
 # def example2():
 # 	X = np.random.gamma(shape=2, scale=1 / 1)
 # 	p = np.exp(-X)
--- proposition1.py
+++ proposition1.py
@@ -1,35 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tqdm
 
 def proposition1():
 	"""
 	tester plein de valeurs de p puis moyenner les K_n ?
 	:return:
 	"""
-	nb_iters_for_p = int(1e1)
-	range_sizes = np.array(1.5 ** np.arange(5, 30)).astype(int)
+	nb_iters_for_p = int(50)
+	range_sizes = np.array(1.5 ** np.arange(5, 35, 3)).astype(int)
 	K_n = np.zeros(len(range_sizes))
-	for _ in range(nb_iters_for_p):
+	for _ in tqdm.trange(nb_iters_for_p):
 		p = np.random.uniform()  # 1e-1  # np.random.uniform()
-		print(f"p={p}")
-		size_sample = int(1e6)
+		# print(f"p={p}")
+		# size_sample = int(1e6)
 		n_iters_for_weigths = int(1e3)
-		w = np.zeros(n_iters_for_weigths)
-		w[0] = p
-		for i in range(1, n_iters_for_weigths):
-			w[i] = (1 - p) * w[i - 1]
+		w = p * (1 - p) ** (np.arange(0, n_iters_for_weigths))  # np.zeros(n_iters_for_weigths)
+		# print(np.sum(w))
+		# w[0] = p
+		# for i in range(1, n_iters_for_weigths):
+		# 	w[i] = (1 - p) * w[i - 1]
 
-		sample = []
+		# sample = []
 		x = []
 		y = []
+
+		sample = np.random.choice(range(n_iters_for_weigths), size=range_sizes[-1], p=w / (np.sum(w)))
 		for size in range_sizes:
-			sample = np.hstack(
-				(sample,
-				 np.random.choice(range(n_iters_for_weigths), size=size, p=w / (np.sum(w)))
-				 )
-			)
-			x.append(len(sample))
-			y.append(len(np.unique(sample)))
+			sub_sample = sample[0:size]
+			x.append(len(sub_sample))
+			y.append(len(np.unique(sub_sample)))
 		K_n = np.vstack(
 			(K_n,
 			 y)
@@ -37,11 +37,21 @@ def proposition1():
 		# unique =
 		# print(len(unique))
 		# np.hstack((np.array(range(10)), np.array(range(4))))
-		print(x)
-		print(y)
+		# print(x)
+		# print(y)
 	K_n = np.delete(K_n, (0), axis=0)
 	print(f"K_n=\n{K_n}")
 	avg_K_n = np.mean(K_n, axis=0)
 
-	plt.plot(np.log(range_sizes), avg_K_n)
+	plt.plot(np.log(range_sizes), avg_K_n,".-",
+
+	         )
+	plt.xlabel("$log(n)$")
+	plt.ylabel("$K_n$")
 	plt.show()
+
+# unique, counts = np.unique(sample, return_counts=True)
+
+# print(dict(zip(unique, counts)))
+
+proposition1()
