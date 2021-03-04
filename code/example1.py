c9ea6b34404a61ca8103be0c0e510efdb6d3eace
Manu0225 <manu.manu34@hotmail.fr>
2021-03-04 18:44:12 +0100

    updated example 1 to sets, added résidus to ex1 and prop1, and spaghetti

--- code/example1.py
+++ code/example1.py
@@ -1,60 +1,94 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from math import floor
 import tqdm
 from sklearn.linear_model import LinearRegression
 
 
-def example1(p):
+def example1(p, nb_iters=20, option=1):
 	"""
 	tester une  valeur de p et moyenner
 	:return:
 	"""
-	nb_iters = int(20)
 	sample_sizes = np.array(1.5 ** np.arange(5, 35, 1)).astype(int)
-	K_n = np.zeros(len(sample_sizes))
-	for _ in tqdm.trange(nb_iters):
-		# n_iters_for_weigths = int(1e3)
-
-		x = []
-		y = []
-
+	print(sample_sizes)
+	K_n = np.zeros((nb_iters, len(sample_sizes)))
+	résidus = np.zeros((nb_iters, len(sample_sizes)))
+	for i in tqdm.trange(nb_iters):
 		sample = np.random.geometric(p,
-		                             size=sample_sizes[-1])
-		for size in sample_sizes:
-			x.append(size)
-			y.append(len(np.unique(sample[0:size])))
-		K_n = np.vstack(
-			(K_n,
-			 y)
-		)
+									 size=sample_sizes[-1])
 
-	K_n = np.delete(K_n, (0), axis=0)
+		set_values = set(sample[0:sample_sizes[0]])
+		K_n[i, 0] = len(set_values)
+		for j in range(1, len(sample_sizes)):
+			set_values |= set(sample[sample_sizes[j - 1]: sample_sizes[j]])
+			K_n[i, j] = len(set_values)
+
+		if option != 1:
+			résidus[i] = (
+			# np.abs(
+			K_n[i] -
+				(
+						np.floor(
+							np.log(sample_sizes * p) / np.abs(np.log(1 - p))
+						)
+						+ 1 + np.euler_gamma / np.abs(np.log(1 - p))
+				)
+			# )
+			)
+
+	# K_n = np.delete(K_n, (0), axis=0)
 	print(f"K_n=\n{K_n}")
-	avg_K_n = np.mean(K_n, axis=0)
+	if option == 1:
+		avg_K_n = np.mean(K_n, axis=0)
+		reg = LinearRegression().fit(np.log(sample_sizes).reshape(len(sample_sizes), 1),
+									 (avg_K_n))
+		m = reg.coef_[0]
+		b = reg.intercept_
 
-	reg = LinearRegression().fit(np.log(sample_sizes).reshape(len(sample_sizes), 1),
-	                             (avg_K_n))
-	m = reg.coef_[0]
-	b = reg.intercept_
+		plt.xscale("log")
 
-	plt.xscale("log")
+		plt.plot(sample_sizes,
+				 avg_K_n,
+				 ".-",
+				 label=f"$K_n$ (moyenné sur {nb_iters} itérations)",
+				 alpha=.9
+				 )
+		plt.plot(sample_sizes,
+				 m * np.log(sample_sizes) + b,
+				 label=f"${m:.2f}x$ (pente attendue : 1/|log(1-p)|≈{abs(np.log(1 - p)) ** -1:.2f})",
+				 alpha=.7
+				 )
+		plt.xlabel("$n$")
+		plt.ylabel("$K_n$")
+		plt.title(f"$K_n$ avec $p={p:.3f}$")
+		plt.legend()
+		plt.show()
+	else:
+		plt.xscale("log")
 
-	plt.plot(sample_sizes,
-	         avg_K_n,
-	         ".-",
-	         label=f"$K_n$ (moyenné sur {nb_iters} itérations)",
-	         alpha=.9
-	         )
-	plt.plot(sample_sizes,
-	         m * np.log(sample_sizes) + b,
-	         label=f"${m:.2f}x$ (pente attendue : 1/|log(1-p)|≈{abs(np.log(1 - p)) ** -1:.2f})",
-	         alpha=.7
-	         )
-	plt.xlabel("$n$")
-	plt.ylabel("$K_n$")
-	plt.title(f"$K_n$ avec $p={p:.3f}$")
-	plt.legend()
-	plt.show()
+		avg_résidus = np.mean(résidus, axis=0)
+		std_résidus = np.std(résidus, axis=0)
+
+		plt.title(f"résidus pour $p={p}$")
+		plt.plot(sample_sizes,
+				 avg_résidus,
+				 ".-",
+				 label=f"résidus (moyennés sur {nb_iters} itérations)",
+				 alpha=.9
+				 )
+		plt.fill_between(sample_sizes,
+						 avg_résidus - std_résidus,
+						 avg_résidus + std_résidus,
+						 alpha=0.3)
+
+		plt.legend()
+		plt.show()
 
 
-example1(np.random.uniform())
+# else:
+
+
+example1(.001,#np.random.uniform(),
+		 nb_iters=100,
+		 option=2)
--- code/proposition1.py
+++ code/proposition1.py
@@ -1,20 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm
+from math import floor
 import tqdm
 from sklearn.linear_model import LinearRegression
 
 
-# import cProfile
-
-
-def nb_unique(a):
-	return len(np.unique(a))
-
-
-nb_unique_v = np.vectorize(nb_unique)
-
-
-def proposition1():
+def proposition1(option=1):
 	"""
 	tester plein de valeurs de p puis moyenner les K_n ?
 	:return:
@@ -22,49 +14,90 @@ def proposition1():
 	nb_iters_for_p = int(5e3)
 	ls_p = np.random.uniform(size=nb_iters_for_p)
 
-	sample_sizes = np.array(1.5 ** np.arange(12, 25, 1)).astype(int)
+	sample_sizes = np.array(1.5 ** np.arange(5, 25, 1)).astype(int)
 
 	K_n = np.zeros((nb_iters_for_p, len(sample_sizes)))
+	résidus = np.zeros((nb_iters_for_p, len(sample_sizes)))
 	for i in tqdm.trange(nb_iters_for_p):
 		sample = np.random.geometric(ls_p[i],
-		                             size=sample_sizes[-1])
+									 size=sample_sizes[-1])
 		set_values = set(sample[0:sample_sizes[0]])
 		K_n[i, 0] = len(set_values)
 		for j in range(1, len(sample_sizes)):
 			set_values |= set(sample[sample_sizes[j - 1]: sample_sizes[j]])
 			K_n[i, j] = len(set_values)
 
+		if option != 1:
+			résidus[i] = (
+				# np.abs(
+					(K_n[i] - 1 / 2 * np.log(sample_sizes) ** 2) / np.log(sample_sizes)
+				# )
+			)
 	print(f"K_n=\n{K_n}")
 
 	avg_K_n = np.mean(K_n, axis=0)
 
-	reg = LinearRegression().fit(np.log(np.log(sample_sizes)).reshape(-1, 1),
-	                             np.log(avg_K_n))
-	# reg = LinearRegression().fit((np.log(sample_sizes) ** 2).reshape(-1, 1),
-	#                              (avg_K_n))
-	m = reg.coef_[0]
-	b = reg.intercept_
+	if option == 1:
 
-	# plt.xscale("log")
+		reg = LinearRegression().fit(np.log(np.log(sample_sizes)).reshape(-1, 1),
+									 np.log(avg_K_n))
 
-	plt.plot(np.log(np.log(sample_sizes)),
-	         np.log(avg_K_n),
-	         ".-",
-	         label="$K_n$ moyen",
-	         alpha=.9
-	         )
-	plt.plot(np.log(np.log(sample_sizes)),
-	         m * np.log(np.log(sample_sizes)) + b,
-	         # label=f"{m:.3f}*x (predicted {abs(np.log(1-p))**-1:.3f})",
-	         label=f"${m:.2f}x$ (pente attendue : 2)",
-	         alpha=.7
-	         )
-	plt.xlabel("$log(log(n))$")
-	plt.ylabel("$log(K_n)$")
-	plt.title(f"$K_n$ moyenné pour {nb_iters_for_p} valeurs de p  ")
-	plt.legend()
-	plt.show()
+		m = reg.coef_[0]
+		b = reg.intercept_
+
+		plt.plot(np.log(np.log(sample_sizes)),
+				 np.log(avg_K_n),
+				 ".-",
+				 label="$K_n$ moyen",
+				 alpha=.9
+				 )
+		plt.plot(np.log(np.log(sample_sizes)),
+				 m * np.log(np.log(sample_sizes)) + b,
+				 label=f"${m:.2f}x$ (pente attendue : 2)",
+				 alpha=.7
+				 )
+		plt.xlabel("$log(log(n))$")
+		plt.ylabel("$log(K_n)$")
+		plt.title(f"$K_n$ moyenné pour {nb_iters_for_p} valeurs de p  ")
+		plt.legend()
+		plt.show()
+	elif option == 2:
+		plt.xscale("log")
+
+		avg_résidus = np.mean(résidus, axis=0)
+		std_résidus = np.std(résidus, axis=0)
+
+		plt.title(f"résidus pour la prop 1")
+		plt.axhline(color="orange",
+					alpha=.7)
+		plt.plot(sample_sizes,
+				 avg_résidus,
+				 ".-",
+				 label=f"résidus (moyennés sur {nb_iters_for_p} itérations)",
+				 alpha=.9
+				 )
+		plt.fill_between(sample_sizes,
+						 avg_résidus - std_résidus,
+						 avg_résidus + std_résidus,
+						 alpha=0.3)
+
+		plt.legend()
+		plt.show()
+	else:
+		# spaghetti plots
+		nb_spaghetti = 10
+		colors = matplotlib.cm.rainbow(np.linspace(0, 1, nb_spaghetti))
+		for i in range(nb_spaghetti):
+			k = floor(np.random.uniform()*nb_iters_for_p)
+			plt.plot(sample_sizes,
+					 K_n[k, :],
+					 ".-",
+					 label=f"trajectoire pour $p={ls_p[k]:.3f}$",
+					 color=colors[i],
+					 alpha=.8
+					 )
+		plt.legend(fontsize=8)
+		plt.show()
 
 
-proposition1()
-# proposition1_option2()
+proposition1(option=2)
